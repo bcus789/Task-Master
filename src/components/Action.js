52de5a56f254dcd5656ef03b11f99666220b0bf1
Charlie NoFace <youremail@gmail.comh.e.ergaster@gmail.comgit config --global user.email youremail@gmail.com>
2020-02-16 11:56:12 -0800

    button styles

--- src/components/Action.js
+++ src/components/Action.js
@@ -2,7 +2,9 @@ import React from "react";
 
 const Action = props => (
   <div>
-    <button onClick={props.handlePick} disabled={!props.hasOptions}>
+    <button 
+    className="big-button"
+    onClick={props.handlePick} disabled={!props.hasOptions}>
       What Is Thy Bidding?
     </button>
   </div>
--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
         exclude: /node_modules/
       },
       {
-        test: /\.s?css$/, //question mark makew scss optional for cs reset compatibility
+        test: /\.s?css$/, //question mark makes scss optional for cs reset compatibility
         use: ["style-loader", "css-loader", "sass-loader"]
       }
     ]
